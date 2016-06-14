cf38d4d9180c7e23ddd4327d5e62ce956e718f40
masterchef8 <masterchef8@gmail.com>
2016-06-14 13:45:42 +0200

    commit safe

--- ACS2.py
+++ ACS2.py
@@ -18,23 +18,51 @@ import matplotlib.pyplot as plt
 
 cons = Constants()
 
+def remove(c: Classifier, set: list):
+    """
+    Remove this classifier from the current set
+    :param c: Classifier to remove
+    :param set:
+    :return:
+    """
+    for i in range(len(set)):
+        if c.condition == set[i].condition and c.action == set[i].action and c.effect == set[i].effect:
+            del set[i]
+            return True
+    return False
 
-def does_match(cl, percept):
+
+def contains(setc: list, c: Classifier):
+    """
+    Check if the classifier is in the set
+    :param setc:
+    :param c:
+    :return:
     """
 
-    :param cl:
+    for i in range(len(setc)):
+        if setc[i] == c:
+            return True
+
+    return False
+
+
+def does_match(c, percept):
+    """
+    Check if classifier's condition match current perception
+    :param c:
     :param percept:
     :return:
     """
     for i in range(len(percept)):
-        if cl.condition[i] != cons.symbol and cl.condition[i] != percept[i]:
+        if c.condition[i] != cons.symbol and c.condition[i] != percept[i]:
             return False
     return True
 
 
 def gen_match_set(pop: list, percept: list):
     """
-
+    Generate a list of Classifier thats match current perception
     :param pop:
     :type pop: list
     :param percept:
@@ -42,7 +70,7 @@ def gen_match_set(pop: list, percept: list):
     :return:
     :rtype: list
     """
-    m = []
+    ma = []
     if time == 0:
         for i in range(cons.nbAction):
             newcl = Classifier()
@@ -53,33 +81,40 @@ def gen_match_set(pop: list, percept: list):
             newcl.t = time
             newcl.q = 0.5
             pop.append(newcl)
-    for cl in pop:
-        if does_match(cl, percept):
-            m.append(cl)
-    return m
+    for c in pop:
+        if does_match(c, percept):
+            ma.append(c)
+    return ma
 
 
-def update_application_average(cl: Classifier, t: int):
+def update_application_average(cli: Classifier, t: int):
     """
-
+    Update Classifier's parameters aav
     :type t: int
     :param t: Time
-    :type cl: Classifier
+    :type cli: Classifier
     """
-    if cl.exp < 1 / cons.beta:
-        cl.aav += (t - cl.tga - cl.aav) / cl.exp
+    if cli.exp < 1 / cons.beta:
+        cli.aav += (t - cli.tga - cli.aav) / cli.exp
     else:
-        cl.aav += cons.beta * (t - cl.tga - cl.aav)
-    cl.tga = t
+        cli.aav += cons.beta * (t - cli.tga - cli.aav)
+    cli.tga = t
 
 
-def does_anticipate_correctly(cl: Classifier, percept: list, percept_: list) -> bool:
+def does_anticipate_correctly(cla: Classifier, percept: list, percept_: list) -> bool:
+    """
+
+    :param cla: Classifier
+    :param percept: Current perception
+    :param percept_: Ancien perception
+    :return:
+    """
     for i in range(cons.lenCondition):
-        if cl.effect != cons.symbol:
+        if cla.effect == cons.symbol:
             if percept_[i] != percept[i]:
                 return False
         else:
-            if cl.effect[i] != percept[i] or percept_[i] == percept[i]:
+            if cla.effect[i] != percept[i] or percept_[i] == percept[i]:
                 return False
     return True
 
@@ -140,19 +175,19 @@ def remove_random_spec_att(l: list):
                 t = False
 
 
-def expected_case(cl: Classifier, percept: list) -> Classifier:
+def expected_case(cli: Classifier, percept: list) -> Classifier:
     """
 
     :rtype: Classifier
     """
-    diff = get_differences(cl.mark, percept)
+    diff = get_differences(cli.mark, percept)
     if diff == [cons.symbol] * cons.lenCondition:
-        cl.q += cons.beta * (1 - cl.q)
+        cli.q += cons.beta * (1 - cli.q)
         return None
     else:
-        spec = number_of_spec(cl.condition)
+        spec = number_of_spec(cli.condition)
         spec_new = number_of_spec(diff)
-        child = copy.copy(cl)
+        child = Classifier(cli)
         if spec == cons.uMax:
             remove_random_spec_att(child.condition)
             spec -= 1
@@ -175,53 +210,56 @@ def expected_case(cl: Classifier, percept: list) -> Classifier:
         return child
 
 
-def unexpected_case(cl: Classifier, percept: list, percept_: list) -> Classifier:
+def unexpected_case(clas: Classifier, percept: list, percept_: list) -> Classifier:
     """
 
     :rtype: Classifier
     """
-    cl.q = cl.q - cons.beta * cl.q
-    cl.mark = percept_
+    assert (len(percept_) == cons.lenCondition), "Wrong leight"
+    assert (len(percept) == cons.lenCondition), "Wrong leight"
+
+    clas.q = clas.q - cons.beta * clas.q
+    clas.mark = percept_
     for i in range(len(percept)):
-        if cl.effect[i] != cons.symbol:
-            if cl.effect[i] != percept_[i] or percept_[i] != percept[i]:
+        if clas.effect[i] != cons.symbol:
+            if clas.effect[i] != percept_[i] or percept_[i] != percept[i]:
                 return None
-    child = copy.copy(cl)
+    child = Classifier(clas)
     for i in range(len(percept)):
-        if cl.effect[i] == cons.symbol and percept_[i] != percept[i]:
+        if clas.effect[i] == cons.symbol and percept_[i] != percept[i]:
             child.condition[i] = percept_[i]
-            child.effect = percept[i]
-    if cl.q < 0.5:
-        cl.q = 0.5
+            child.effect[i] = percept[i]
+    if clas.q < 0.5:
+        clas.q = 0.5
     child.exp = 1
     return child
 
 
-def add_alp_classifier(cl: Classifier, pop: list, aset: list):
+def add_alp_classifier(classifier: Classifier, pop: list, aset: list):
     oldcl = None
     for c in aset:
-        if c.isSubsumer(cl):
-            if oldcl is None or c.isMoreGeneral(cl):
+        if c.isSubsumer(classifier):
+            if oldcl is None or c.isMoreGeneral(classifier):
                 oldcl = c
     if oldcl is None:
         for c in aset:
-            if c.equals(cl):
+            if c.equals(classifier):
                 oldcl = c
     if oldcl is None:
-        pop.append(cl)
-        aset.append(cl)
+        pop.append(classifier)
+        aset.append(classifier)
     else:
         oldcl.q += cons.beta * (1 - oldcl.q)
 
 
-def cover_triple(percept_: list, act: int, percept: list, t: int) -> Classifier:
+def cover_triple(percept_: list, action: int, percept: list, t: int) -> Classifier:
     child = Classifier()
 
     for i in range(len(percept)):
         if percept_[i] != percept[i]:
             child.condition[i] = percept_[i]
             child.effect[i] = percept[i]
-    child.action = act
+    child.action = action
     child.exp = 0
     child.r = 0
     child.aav = 0
@@ -233,11 +271,11 @@ def cover_triple(percept_: list, act: int, percept: list, t: int) -> Classifier:
     return child
 
 
-def apply_alp(aset: list, percept_: list, percept: list, t: int, pop: list):
+def apply_alp(actionset: list, percept_: list, percept: list, t: int, pop: list, action: int):
     """
-
-    :param aset:
-    :type aset: list
+    :param action:
+    :param actionset:
+    :type actionset: list
     :param percept_:
     :type percept_: list
     :param percept:
@@ -247,81 +285,84 @@ def apply_alp(aset: list, percept_: list, percept: list, t: int, pop: list):
     :param pop:
     :type pop: list
     """
-    wasExpectedCase = 0
-    for cl in aset:
-        cl.exp += 1
-        update_application_average(cl, t)
+    assert (len(percept_) == cons.lenCondition), "Wrong length"
+    assert (len(percept) == cons.lenCondition), "Wrong length"
 
-        if does_anticipate_correctly(cl, percept, percept_):
-            newcl = expected_case(cl, percept)
+    wasExpectedCase = 0
+    for c in actionset:
+        contains(pop, c)
+        c.exp += 1
+        update_application_average(c, t)
+        if does_anticipate_correctly(c, percept, percept_):
+            newcl = expected_case(c, percept)
             wasExpectedCase += 1
         else:
-            newcl = unexpected_case(cl, percept, percept_)
-            if cl.q < cons.thetaI:
-                aset.remove(cl)
-                pop.remove(cl)
+            newcl = unexpected_case(c, percept, percept_)
+            if c.q < cons.thetaI:
+                # if c.gold is False:
+                remove(c, Pop)
+                actionset.remove(c)
 
         if newcl is not None:
             newcl.tga = t
-            add_alp_classifier(newcl, pop, aset)
+            add_alp_classifier(newcl, pop, actionset)
     if wasExpectedCase == 0:
-        newcl = cover_triple(percept_, act, percept, t)
-        add_alp_classifier(newcl, pop, aset)
+        newcl = cover_triple(percept_, action, percept, t)
+        add_alp_classifier(newcl, pop, actionset)
 
 
-def get_max_for_rl(m: list) -> int:
+def get_max_for_rl(matchset: list) -> int:
     """
 
-    :param m:
+    :param matchset:
     :return:
     """
     sumrl = 0
-    for cl in m:
-        if cl.effect != [cons.symbol] * cons.lenCondition:
-            sumrl += cl.q * cl.r
-    return int(sumrl)
+    for c in matchset:
+        if c.effect != [cons.symbol] * cons.lenCondition:
+            sumrl += c.q * c.r
+    return sumrl
 
 
-def apply_rl(aset: list, p: int):
+def apply_rl(aset: list, rwd: int):
     """
 
-
     :param aset:
-    :param p:
+    :param rwd:
     :param maxp:
     :type maxp: int
     """
     maxp = get_max_for_rl(M)
-    for cl in aset:
-        cl.r += cons.beta * (p + cons.gamma * maxp - cl.r)
-        cl.ir += cons.beta * (p - cl.ir)
+    for classifier in aset:
+        classifier.r += cons.beta * (rwd + cons.gamma * maxp - classifier.r)
+        classifier.ir += cons.beta * (rwd - classifier.ir)
 
 
 def select_offspring(aset: list) -> Classifier:
     qualitySum = 0
-    for cl in aset:
-        qualitySum += cl.q ** 3
+    for cla in aset:
+        qualitySum += cla.q ** 3
     choicePoint = random() * qualitySum
     qualitySum = 0
-    for cl in aset:
-        qualitySum += cl.q ** 3
+    for cla in aset:
+        qualitySum += cla.q ** 3
         if qualitySum > choicePoint:
-            return cl
+            return cla
 
 
-def apply_ga_mutation(cl: Classifier):
-    for i in range(len(cl.condition)):
-        if cl.condition[i] != cons.symbol:
+def apply_ga_mutation(classifier: Classifier):
+    for i in range(len(classifier.condition)):
+        if classifier.condition[i] != cons.symbol:
             if random() < cons.mu:
-                cl.condition[i] = cons.symbol
+                classifier.condition[i] = cons.symbol
 
 
 def apply_crossover(cl1: Classifier, cl2: Classifier):
     if cl1.effect != cl2.effect:
         return
-    x = random()*(len(cl1.condition)+1)
+    x = random() * (len(cl1.condition) + 1)
     while True:
-        y = random()*(len(cl1.condition)+1)
+        y = random() * (len(cl1.condition) + 1)
         if x != y:
             break
 
@@ -335,52 +376,53 @@ def apply_crossover(cl1: Classifier, cl2: Classifier):
             tp = cl1.condition[i]
             cl1.condition[i] = cl2.condition[i]
             cl2.condition[i] = tp
-        i+=1
+        i += 1
         if i <= y:
             break
 
+
 def delete_classifier(aset: list, pop: list):
-    sum = 0
+    summation = 0
     for c in pop:
-        sum += c.num
-    while cons.inSize + sum > cons.thetaAS:
+        summation += c.num
+    while cons.inSize + summation > cons.thetaAS:
         cldel = None
-        for cl in pop:
-            if random() < 1/3:
+        for classifier in pop:
+            if random() < 1 / 3:
                 if cldel is None:
-                    cldel = cl
+                    cldel = classifier
                 else:
-                    if cl.q - cldel.q < -0.1:
-                        cldel = cl
-                    if abs(cl.q - cldel.q) <= 0.1:
-                        if cl.mark is not None and cldel.mark is None:
-                            cldel = cl
-                        elif cl.mark is not None or cldel.mark is None:
-                            if cl.aav > cldel.aav:
-                                cldel = cl
+                    if classifier.q - cldel.q < -0.1:
+                        cldel = classifier
+                    if abs(classifier.q - cldel.q) <= 0.1:
+                        if classifier.mark is not None and cldel.mark is None:
+                            cldel = classifier
+                        elif classifier.mark is not None or cldel.mark is None:
+                            if classifier.aav > cldel.aav:
+                                cldel = classifier
 
         if cldel is not None:
             if cldel.num > 1:
                 cldel.num -= 1
             else:
-                aset.remove(cl)
-                pop.remove(cl)
+                if classifier.gold is False:
+                    pop.remove(classifier)
+                    remove(classifier, aset)
 
 
-
-def add_ga_classifier(aset: list, pop: list, cl: Classifier):
+def add_ga_classifier(aset: list, pop: list, classifier: Classifier):
     oldcl = None
     for c in aset:
-        if c.isSubsumer(cl):
-            if oldcl is None or c.isMoreGeneral(cl):
+        if c.isSubsumer(classifier):
+            if oldcl is None or c.isMoreGeneral(classifier):
                 oldcl = c
     if oldcl is None:
         for c in aset:
-            if c.equals(cl):
+            if c.equals(classifier):
                 oldcl = c
     if oldcl is None:
-        pop.append(cl)
-        aset.append(cl)
+        pop.append(classifier)
+        aset.append(classifier)
     else:
         if oldcl.mark is None:
             oldcl.num += 1
@@ -389,17 +431,17 @@ def add_ga_classifier(aset: list, pop: list, cl: Classifier):
 def apply_ga(aset: list, t: int, pop: list):
     sumNum = 0
     sumTgaN = 0
-    for cl in aset:
-        sumTgaN += cl.tga * cl.num
-        sumNum += cl.num
-    if t - sumTgaN / sumNum > cons.thetaGA:
-        for cl in aset:
-            cl.tga = t
+    for CL in aset:
+        sumTgaN += CL.tga * CL.num
+        sumNum += CL.num
+    if (t - sumTgaN) / sumNum > cons.thetaGA:
+        for CL in aset:
+            CL.tga = t
         parent1 = select_offspring(aset)
         parent2 = select_offspring(aset)
 
-        child1 = copy.copy(parent1)
-        child2 = copy.copy(parent2)
+        child1 = Classifier(parent1)
+        child2 = Classifier(parent2)
 
         child1.num += 1
         child2.num += 1
@@ -421,14 +463,10 @@ def apply_ga(aset: list, t: int, pop: list):
         child2.q /= 2
 
         delete_classifier(aset, pop)
-        if child1.condition == [cons.symbol]*cons.lenCondition:
-            pass
-        else:
+        if child1.condition != [cons.symbol] * cons.lenCondition:
             add_ga_classifier(aset, pop, child1)
 
-        if child2.condition == [cons.symbol] * cons.lenCondition:
-            pass
-        else:
+        if child2.condition != [cons.symbol] * cons.lenCondition:
             add_ga_classifier(aset, pop, child2)
 
 
@@ -437,30 +475,27 @@ def choose_action(aset: list):
         c = choice([i for i in range(cons.nbAction)])
         return c
     else:
-        bestCL = Classifier()
-        for cl in aset:
-            if cl.effect != [cons.symbol] * cons.lenCondition:
-                bestCL = cl
-                break
-        for cl in aset:
-            if cl.effect != [cons.symbol] * cons.lenCondition and cl.q * cl.r > bestCL.q * bestCL.r:
-                bestCL = cl
+        bestCL = aset[0]
+        for classifierr in aset:
+            if classifierr.effect != [cons.symbol] * cons.lenCondition and classifierr.q * classifierr.r > bestCL.q * bestCL.r:
+                bestCL = classifierr
         return bestCL.action
 
 
-def gen_action_set(aset: list, act: int):
+def gen_action_set(aset: list, action: int):
     """
 
-    :param act: action
-    :type act: int
+    :param action: action
+    :type action: int
     :param aset: Match set
     :type aset: list
     :rtype: list
     """
     a = []
-    for cl in aset:
-        if cl.action == act:
-            a.append(cl)
+
+    for classifier in aset:
+        if classifier.action == action:
+            a.append(classifier)
     return a
 
 
@@ -480,50 +515,51 @@ if __name__ == '__main__':
     time = 0
     p = 0
     act = None
-    m=0
+    m = 0
+    perception = env.perception
     while True:
-        perception = env.perception
 
         M = gen_match_set(Pop, perception)
-        if len(A) == 0:
-            pass
         if A_ is not None:
-            apply_alp(A_, perception_, perception, time, Pop)
+            apply_alp(A_, perception_, perception, time, Pop, act)
             apply_rl(A_, p)
             apply_ga(A_, time, Pop)
-
+        A_ = None
         act = choose_action(M)
         A = gen_action_set(M, act)
         p = env.execute_action(act)
         time += 1
         perception_ = perception
         perception = env.perception
-
-        if env.flag:
-            apply_alp(A, perception_, perception, time, Pop)
-            # noinspection PyTypeChecker
-            apply_rl(A, p)
-            apply_ga(A, time, Pop)
         A_ = A
+        if env.flag:
+
+            apply_alp(A, perception_, perception, time, Pop, act)
+
+            apply_rl(A, p)
+
+            apply_ga(A, time, Pop)
+
+
         if env.flag:
             env.stop()
             A_ = None
+        # ------------------------------------------------------
 
-        if time % 100 == 0:
+        if time % 1 == 0:
             perfClasseur.append(len(Pop))
             m = 0
             for cl in Pop:
                 m += cl.q
             m /= len(Pop)
             meanQ.append(m)
-            print(m)
+            print(m, len(Pop))
         if time == 10000:
             break
 
     print(len(Pop))
     for cl in Pop:
-        print('c : ', cl.condition, " a : ", cl.action, " e ", cl.effect, " q ", cl.q, " exp ", cl.exp,
-              " age : ", cl.t)
+        print(cl.condition, cl.action, cl.effect, cl.q, cl.t)
 
     plt.plot(perfClasseur)
     plt.ylabel('Numbers of Classifier')
